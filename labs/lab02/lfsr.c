a504829d8a3f3c577753286a6f1190788da6b87b
LCJD <1721404800@qq.com>
2024-04-04 23:44:51 +0800

    lab2 exercise2 finish

--- labs/lab02/lfsr.c
+++ labs/lab02/lfsr.c
@@ -5,6 +5,12 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    uint8_t flag = (*reg) & 1;
+    flag ^= ((*reg) >> 2) & 1;
+    flag ^= ((*reg) >> 3) & 1;
+    flag ^= ((*reg) >> 5) & 1;
+    (*reg) >>= 1;
+    uint8_t bit = ((*reg) >> 15) & 1;
+    (*reg) ^= ((bit ^ flag) << 15);
 }
 
